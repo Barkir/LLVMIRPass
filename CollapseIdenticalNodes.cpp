dc6d104e2512d7099ddd5fc9b7d1a1644bb181ee
Barkir <r.barinov2006@gmail.com>
2025-11-10 18:03:07 +0300

    Changed a pass

--- CollapseIdenticalNodes.cpp
+++ CollapseIdenticalNodes.cpp
@@ -15,6 +15,14 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Passes/PassPlugin.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/IR/CFG.h"
 
 #define DEBUG_TYPE "collapse-nodes"
 
@@ -26,66 +34,59 @@
 
 using namespace llvm;
 
-bool endsWithConditionalBranch(BasicBlock *BB) {
-    auto *term = BB->getTerminator();
-    if (auto *BI = dyn_cast<BranchInst>(term)) {
-        return BI->isConditional();
-    }
-
-    return false;
-}
-
-// bool compareConditions(BasicBlock *BB) {
-//     auto *leftBB = BB->getSuccessor(0);     // getting left branch
-//     auto *rightBB = BB->getSuccessor(1);    // getting right branch
-//
-//     for (auto *instruction : leftBB) {
-//
-//     }
-// }
-
 using valueHashTab = std::unordered_map<Value*, int32_t>;
 
 int CountValueRecursively(Value *val, valueHashTab& tab) {
+    errs() << UCYN "CountValueRecursively" RESET << "\n";
     int retVal = 0;
 
-    auto value = tab[val];
-    if (tab[val] != tab.end()) {
-        return value;
+    auto value = tab.find(val);
+    if (value != tab.end()) {
+        errs() << "Found value in hashtable " << BYEL << value->second << RESET "\n";
+        return value->second;
     }
     if (auto intVal = dyn_cast<ConstantInt>(val)) {
+        errs() << "The from instruction is " << BYEL << value->second << RESET "\n";
         return intVal->getSExtValue();
     }
-    int numOperands = val->getNumOperands();
-    for (int i = 0; i < numOperands; i++) {
-        auto *instr = val->getOperand(i)
-        switch (instr->getOpcode()) {
-            case Instruction::Add:
-                retVal = CountValueRecursively(instr->getOpcode(0), tab) + CountValueRecursively(instr->getOpcode(1), tab);
-                break;
 
-            case Instruction::Sub:
-                retVal = CountValueRecursively(instr->getOpcode(0), tab) - CountValueRecursively(instr->getOpcode(1), tab);
-                break;
+    for (auto User : val->users()) {
+        errs() << "val: " << *val << "||| User is " << *User << "\n";
+        if (auto *instr = dyn_cast<BinaryOperator>(User)) {
+            switch (instr->getOpcode()) {
+                case Instruction::Add:
+                    errs() << YEL "ADD" RESET "\n";
+                    retVal = CountValueRecursively(instr->getOperand(0), tab) + CountValueRecursively(instr->getOperand(1), tab);
+                    break;
 
-            case Instruction::Mul:
-                retVal = CountValueRecursively(instr->getOpcode(0), tab) * CountValueRecursively(instr->getOpcode(1), tab);
-                break;
+                case Instruction::Sub:
+                    errs() << YEL "SUB" RESET "\n";
+                    retVal = CountValueRecursively(instr->getOperand(0), tab) - CountValueRecursively(instr->getOperand(1), tab);
+                    break;
 
-            case Instruction::Div:
-                retVal = CountValueRecursively(instr->getOpcode(0), tab) / CountValueRecursively(instr->getOpcode(1), tab);
-                break;
+                case Instruction::Mul:
+                    errs() << YEL "MUL" RESET "\n";
+                    retVal = CountValueRecursively(instr->getOperand(0), tab) * CountValueRecursively(instr->getOperand(1), tab);
+                    break;
+
+                case Instruction::SDiv:
+                    errs() << YEL "DIV" RESET "\n";
+                    retVal = CountValueRecursively(instr->getOperand(0), tab) / CountValueRecursively(instr->getOperand(1), tab);
+                    break;
             }
+        }
     }
+
     return retVal;
 }
 
 int GetConstantFromPhiNode(Instruction *phi) {
+    errs() << UCYN "GetConstantFromPhiNode" RESET << "\n";
     auto *phiNode = dyn_cast<PHINode>(phi);
     if (phiNode) {
-        int numValues = phiInstr->getNumIncomingValues();
+        int numValues = phiNode->getNumIncomingValues();
         for (int i = 0; i < numValues; i++) {
-            if (auto *constantVal = dyn_cast<ConstantInt>(phiInstr->getIncomingValue(i))) {
+            if (auto *constantVal = dyn_cast<ConstantInt>(phiNode->getIncomingValue(i))) {
                 return constantVal->getSExtValue();
             }
         }
@@ -93,46 +94,71 @@ int GetConstantFromPhiNode(Instruction *phi) {
 }
 
 Instruction* PhiNodeWithConstant(BasicBlock& BB) {
+    errs() << UCYN "PhiNodeWithConstant" RESET << "\n";
     for (auto &I : BB) {
+        errs() << BYEL "Got instruction " << I <<  RESET "\n";
         if (auto *phiInstr = dyn_cast<PHINode>(&I)) {
+            errs() << "Found phi node " << *phiInstr << "\n";
             int numValues = phiInstr->getNumIncomingValues();
             for (int i = 0; i < numValues; i++) {
                 if (auto *constantVal = dyn_cast<ConstantInt>(phiInstr->getIncomingValue(i))) {
+                    errs() << "Phi node has a constant. Returning it as an Instruction*" << "\n";
+                    errs() << UCYN "END OF FUNC" RESET << "\n";
                     return &I;
                 }
             }
         }
     }
+    errs() << UCYN "END OF FUNC" RESET << "\n";
+    return nullptr;
 }
 
-valueHashTab& collectICmp(Function &F) {
+valueHashTab collectICmp(Function &F) {
+    errs() << UCYN "collectICmp" RESET << "\n";
     valueHashTab instrMap;
     for (auto &BB : F) {
         for (auto &I : BB) {
             if (I.getOpcode() == Instruction::ICmp) {
+                errs() << BYEL "Found icmp! " << I << RESET "\n";
                 auto *castI = dyn_cast<ICmpInst>(&I);
                 if(castI->getPredicate() == CmpInst::ICMP_EQ) {
+                    errs() << "That is icmp eq!" << "\n";
                     auto *firstOperand  = I.getOperand(0);
                     auto *secondOperand = I.getOperand(1);
                     if (auto *secondOperandConst = dyn_cast<ConstantInt>(secondOperand)) {
+                        errs() << BYEL << "second op is num! ";
                         const int32_t sextVal = secondOperandConst->getSExtValue();
+                        errs() << CYNHB "sextVal is " << sextVal << RESET "\n";
                         instrMap[firstOperand] = sextVal;
+                    } else if (auto *firstOperandConst = dyn_cast<ConstantInt>(firstOperand)) {
+                        errs() << BYEL << "first op is num! ";
+                        const int32_t sextVal = firstOperandConst->getSExtValue();
+                        errs() << CYNHB "sextVal is " << sextVal << RESET "\n";
+                        instrMap[secondOperand] = sextVal;
                     }
                 }
             }
         }
     }
+    errs() << UCYN "END OF FUNC" RESET << "\n";
     return instrMap;
 }
 
 PreservedAnalyses CollapseIdenticalNodesPass::run(Function &F, FunctionAnalysisManager &AM) {
 
+    errs() << "Starting CollapseIdenticalNodesPass..." << "\n";
+
     for (auto &BB : F) {
         if (auto *phiNode = PhiNodeWithConstant(BB)) {
             valueHashTab tab = collectICmp(F);
-            auto value = CountValueRecursively(phiNode, tab);
+            errs() << "collected icmp and got hashtab!" << "\n";
+            // auto *phiVal = dyn_cast<Value>(phiNode);
+            auto *phi = dyn_cast<PHINode>(phiNode);
+            auto value = CountValueRecursively(phi->getIncomingValue(1), tab);
+            errs() << MAGB "Total Value is " << value << RESET "\n";
             if (value == GetConstantFromPhiNode(phiNode)) {
-                DeprecateConstantFromPhiNode(phiNode);
+                // DeprecateConstantFromPhiNode(phiNode);
+                errs() << "WOW" << "\n";
                 return PreservedAnalyses::all();
             }
         }
